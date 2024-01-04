7190064c525307393225a30cfab99c0f294cc323
Jorge De León <dele19817@uvg.edu.gt>
2024-01-04 13:58:24 -0600

    Ejemplo de cómo correr tests de gin en un pre-commit hook

---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"os"
+	"log"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/gin-gonic/gin"
+)
+
+func TestPingRoute(t *testing.T) {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/albums", nil)
+	router.ServeHTTP(w, req)
+
+	dat, err := os.ReadFile("getAlbumsResult.json")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, string(dat), w.Body.String())
+}
